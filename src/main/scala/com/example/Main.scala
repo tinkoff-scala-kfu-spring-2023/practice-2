59b3d88f7b931a83142d2a3f4e4e3c0655ffe786
G1ng3r <saynulanude@gmail.com>
2023-03-03 15:18:16 +0300

    practice 3 in class

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,36 +1,63 @@
 package com.example
 
+import scala.util.Try
+
 
 object Main {
   def main(args: Array[String]): Unit = {
     val input = io.Source.fromFile(args(0))
-    val commands = input.getLines().map { line =>
+    val commands = input.getLines().zipWithIndex.map { case (line, idx) =>
       val splitted = line.split(";")
-      splitted.headOption.map {
-        case "move"   => Move(splitted(1), splitted(2).toInt)
-        case "load"   => Load(Goods(splitted(1).toInt, splitted(2).toInt))
-        case "unload" => Unload(Goods(splitted(1).toInt, splitted(2).toInt))
-      }.get
+      val mayBeCommand = splitted.headOption.map {
+        case "move"   => modifyError(Try {
+          val direction = splitted(1)
+          val distance = splitted(2).toInt
+          Move(direction, distance)
+        }, idx)
+        case "load"   => modifyError(Try(Load(Goods(splitted(1).toInt, splitted(2).toInt))), idx)
+        case "unload" => modifyError(Try(Unload(Goods(splitted(1).toInt, splitted(2).toInt))), idx)
+        case _ => Left(s"$line has no command")
+      }
+      if (mayBeCommand.isEmpty) Left(s"$idx line is empty")
+      else mayBeCommand.get
+    }.toArray
+    val (errors, success) = split(commands, Array(), Array())
+    if (errors.nonEmpty) println(s"ACHTUNG: ${errors.mkString("\n")}")
+    else {
+      val robot = Robot(Array(), Array())
+      val result = runCommands(Right(robot), success)
+      result match {
+        case Right(robot) => robot.madeMoves.foreach(println)
+        case Left(error) => println(s"ERROR! ${error.getMessage}")
+      }
     }
-    val robot = Robot(Array(), Array())
-    val result = runCommands(robot, commands.toArray)
-    result.madeMoves.foreach(println)
   }
 
-  def runCommands(robot: Robot, commands: Array[Command]): Robot = {
+  def modifyError(attempt: Try[Command], idx: Int): Either[String, Command] =
+    attempt.toEither.left.map(ex => s"LINE $idx: " + ex.getMessage)
+
+  def split(array: Array[Either[String, Command]], error: Array[String], success: Array[Command]): (Array[String], Array[Command]) = {
+    if (array.isEmpty) (error, success)
+    else array.head match {
+      case Left(exception) => split(array.tail, error.appended(exception), success)
+      case Right(value) => split(array.tail, error, success.appended(value))
+    }
+  }
+
+  def runCommands(robotOr: Either[Throwable, Robot], commands: Array[Command]): Either[Throwable, Robot] = {
     if (commands.isEmpty)
-      robot
+      robotOr
     else
       commands.head match {
-        case Load(goods)   => runCommands(robot.load(goods), commands.tail)
-        case Unload(goods) => runCommands(robot.unload(goods), commands.tail)
+        case Load(goods)   => runCommands(robotOr.flatMap(_.load(goods)), commands.tail)
+        case Unload(goods) => runCommands(robotOr.flatMap(_.unload(goods)), commands.tail)
         case Move(direction, length) =>
-          runCommands(robot.go(direction, length), commands.tail)
+          runCommands(robotOr.flatMap(_.go(direction, length)), commands.tail)
       }
   }
 }
 
-trait Command
+trait Command extends Product with Serializable
 
 case class Load(goods: Goods) extends Command
 case class Unload(goods: Goods) extends Command
--- src/main/scala/com/example/Robot.scala
+++ src/main/scala/com/example/Robot.scala
@@ -2,20 +2,23 @@ package com.example
 
 case class Robot(carry: Array[Goods], madeMoves: Array[Move]) {
   private val maxWeight = 1000
-  def load(goods: Goods): Robot = {
-    if ((carry.map(_.mass).sum + goods.mass) > maxWeight) throw new RuntimeException("I can't carry anymore")
-    println(s"Loading ${goods.mass}")
-    copy(carry appended goods, madeMoves)
+  def load(goods: Goods): Either[Throwable, Robot] = {
+    if ((carry.map(_.mass).sum + goods.mass) > maxWeight)
+      Left(new RuntimeException("I can't carry anymore"))
+    else {
+      println(s"Loading ${goods.mass}")
+      Right(copy(carry appended goods, madeMoves))
+    }
   }
 
-  def unload(goods: Goods): Robot = {
-    println()
+  def unload(goods: Goods): Either[Throwable, Robot] = {
+    println(s"unloading $goods")
     ???
   }
 
-  def go(direction: String, length: Int): Robot = {
+  def go(direction: String, length: Int): Either[Throwable, Robot] = {
     println(s"Moving $direction $length cm")
-    copy(madeMoves = madeMoves.appended(Move(direction, length)))
+    Right(copy(madeMoves = madeMoves.appended(Move(direction, length))))
   }
 }
 
